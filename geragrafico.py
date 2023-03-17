71ca817a07f155e5dd0104ef3ef81ad4b86990c1
Nicolas Saraiva <nicolasbrasilrs@gmail.com>
2023-03-17 15:57:06 -0300

    a

--- geragrafico.py
+++ geragrafico.py
@@ -10,8 +10,12 @@ def main():
     plt.title('Gráfico de linhas')
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
+    i = 1
     for serie in valores:
-       plt.plot(serie)
+        plt.plot(serie, label='Série ' + str(i))   
+        i += 1
+    plt.legend(loc='upper left')
+
     plt.show()
     
 main()
